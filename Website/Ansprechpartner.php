8eed372abe613f4fa522af13f07ecd4c453f52c1
Denetor03 <96008824+Denetor03@users.noreply.github.com>
2022-11-24 10:39:58 +0100

    Add files via upload

--- Website/Ansprechpartner.php
+++ Website/Ansprechpartner.php
@@ -1,46 +1,27 @@
-<!DOCTYPE html>
-<html lang="de">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.0/css/fontawesome.min.css" integrity="sha384-z4tVnCr80ZcL0iufVdGQSUzNvJsKjEtqYZjiQrrYKlpGow+btDHDfQWkFjoaz/Zr" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="style.css" /> 
-    <title>Document</title>
-</head>
-<body>
-    <div class="container px-0 mx-0">
-        <div class="d-flex justify-content-between px-5" id="header">
-            <h1>
-                Schule-xy
-            </h1>
-            <button onclick='window.location="signIn.html"'; class="rounded-pill my-1 px-5" id="login-button">
-                Login
-            </button>
-        </div>
-    </div>
-    <div class="py-5 mt-5 d-flex justify-content-center" id="ansprechpartner">
-        <div class="w-50">
-            <h1>Ansprech partner</h1><hr>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "Elternsprechtag";
 
-            <div class="d-flex justify-content-between">
-                <label for="bezeichnung">Veranstaltungs Bezeichnung</label>
-                <input type="text" id="bezeichnung">
-            </div>
-            <hr>
-            <div class="d-flex justify-content-between">    
-                <label for="start">Verantstaltungs Datum</label>
-                <input type="date" id="start" name="Start-date">
-            </div>
-            <br>
-            <div class="d-flex justify-content-between">
-                <button onclick="window.location='SignIn.html'" value="Zurück">Zurück</button>
-                <button type="submit">Erstellen</button>
-            </div>
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        </div>
-    </div>
-</body>
-</html>
+  // prepare sql and bind parameters
+  $stmt = $conn->prepare("INSERT INTO tbl_ansprechpartner )
+  VALUES (:PK_ansprechpartner, :Vorname, :Nachname, :Betriebsname, :`E-mail`, :Mobilnummer)");
+  $stmt->bindParam(':Kuerzel', $_POST["kuerzel"]);
+  $stmt->bindParam(':Geschlecht`', $_POST["Geschlecht"]);
+  $stmt->bindParam(':`E-mail`', $_POST["Email"]);
+  $stmt->bindParam(':Vorname', $_POST["Vorname"]);
+  $stmt->bindParam(':Nachname`', $_POST["Nachname"]);
+  $stmt->execute();
+
+  echo "New records created successfully";
+} catch(PDOException $e) {
+  echo "Error: " . $e->getMessage();
+}
+$conn = null;
+?> 
